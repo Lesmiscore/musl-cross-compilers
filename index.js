53505adf57b4f5fd31817b2badca80d834d4c855
nao20010128nao <nao20010128@gmail.com>
2020-10-15 01:10:31 +0000

    ビルドモード
    無かった時用

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const core = require("@actions/core");
 const tc = require("@actions/tool-cache");
+const exec = require("@actions/exec");
+const io = require("@actions/io");
 const path = require("path");
 
 const target = core.getInput("target", { required: true });
 const variant = core.getInput("variant", { required: true });
+const build=core.getInput("build").toUpperCase()==="TRUE";
 
 (async () => {
   try {
@@ -12,9 +15,40 @@ const variant = core.getInput("variant", { required: true });
       "_"
     )}.tar.gz`;
 
-    let cachedPath = tc.find("mcm", `${target}-${variant}.tar.gz`);
+    let cachedPath ;
+    if(build){
+      const destDir=path.join("/opt/",target,variant);
+      await io.mkdirP(destDir);
+      let ret=await exec.exec("git",["clone",`https://github.com/${variant}.git`,destDir]);
+      if(ret!=0){
+        throw new Error(`git clone failed with code ${ret}`);
+      } 
+      
+      ret=await exec.exec("make",["-j4"],{
+        cwd:destDir,
+        env:{
+          TARGET:target
+        }
+      });
+      if(ret!=0){
+        throw new Error(`make -j4 failed with code ${ret}`);
+      } 
+      
+      ret=await exec.exec("make",["install"],{
+        cwd:destDir,
+        env:{
+          TARGET:target
+        }
+      });
+      if(ret!=0){
+        throw new Error(`make install failed with code ${ret}`);
+      } 
+      cachedPath=destDir;
+    }else{
+      cachedPath= tc.find("mcm", `${target}-${variant}.tar.gz`);
+    }
     if (cachedPath) {
-      console.log(`Found cache at ${cachedPath}`);
+        console.log(`Found installation at ${cachedPath}`);
     } else {
       const toolchainPath = await tc.downloadTool(url);
       const toolchainExtractedFolder = await tc.extractTar(toolchainPath);
